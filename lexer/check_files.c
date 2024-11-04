e4201e2ecfe29addea8eb8ce273b20106eae68c2
joandre- <joandre-@student.42lisboa.com>
2024-11-04 22:11:53 +0000

    fix for check_files not throwing error when stdin file redirection does not exist

--- lexer/check_files.c
+++ lexer/check_files.c
@@ -6,7 +6,7 @@
 /*   By: vamachad <vamachad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:00:19 by vamachad          #+#    #+#             */
-/*   Updated: 2024/11/04 03:36:43 by joandre-         ###   ########.fr       */
+/*   Updated: 2024/11/04 22:08:58 by joandre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,18 @@ bool	check_files(t_token *token)
 {
 	while (token)
 	{
-		if (token->type == RED_IN)
-			return (open_file(token, O_RDONLY, 0));
+		if (token->type == RED_IN && access(token->next->str, R_OK) != 0)
+			return (minishell_errmsg(token->next->str, strerror(errno)), false);
 		else if (token->type == APPEND)
-			return (open_file(token, O_WRONLY | O_CREAT | O_APPEND, 0644));
+		{
+			if (!open_file(token, O_WRONLY | O_CREAT | O_APPEND, 0644))
+				return (false);
+		}
 		else if (token->type >= RED_OUT)
-			return (open_file(token, O_WRONLY | O_CREAT | O_TRUNC, 0644));
+		{
+			if (!open_file(token, O_WRONLY | O_CREAT | O_TRUNC, 0644))
+				return (false);
+		}
 		token = token->next;
 	}
 	return (true);
